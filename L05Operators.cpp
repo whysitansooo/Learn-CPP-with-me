6142a93ebf2a6a152566767c0302af867d14749a
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2023-06-06 20:36:29 +0530

    Add files via upload

---
+++ L05Operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int f;
+    cout<<"Enter Fah value: ";
+    cin>>f;
+    int c = (5/9.0)*(f-32);
+    cout<<"In Celsius: "<<c;
+    
+    //Checking if two number are equal 
+    int a,b;
+    cin>>a>>b;
+    bool c = (a==b);
+    cout<<c; 
+}
