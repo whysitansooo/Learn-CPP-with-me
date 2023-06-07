0d9c383e7e28c5b1cfdb863d56ba5195077121dd
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2023-06-08 00:34:08 +0530

    Learn about Conditional statements

--- L06Conditionals.cpp
+++ L06Conditionals.cpp
@@ -1,64 +1,64 @@
 //? Checking if two numbers are equal or not
 
-//  #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int a,b;
-//     cout<<"Enter your numbers: ";
-//     cin>>a>>b;
+ #include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter your numbers: ";
+    cin>>a>>b;
     
-//     if(a==b){
-//         cout<<"Hey these are equal";
-//     }
-//     else{
-//         cout<<"Not equal";
-//     }
-// } 
+    if(a==b){
+        cout<<"Hey these are equal";
+    }
+    else{
+        cout<<"Not equal";
+    }
+} 
 
 //? Multiple conditions
-// #include <iostream>
-// using  namespace std;
+#include <iostream>
+using  namespace std;
 
-// int main()
-// {
-//     int a,b;
-//     cout<<"Enter your numbers: ";
-//     cin>>a>>b;
+int main()
+{
+    int a,b;
+    cout<<"Enter your numbers: ";
+    cin>>a>>b;
 
-//     if(a==b) cout<<"Both are equal";
-//     else if(a<b) cout<<"a is smaller";
-//     else cout<<"a is greater";
+    if(a==b) cout<<"Both are equal";
+    else if(a<b) cout<<"a is smaller";
+    else cout<<"a is greater";
             
     
 // }
 
 //? Even odd check
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int a;
-//     cout<<"Enter your number: ";
-//     cin>>a;
-//     if(a%2==0) cout<<"Even";
-//     else cout<<"ODD";
-// }
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter your number: ";
+    cin>>a;
+    if(a%2==0) cout<<"Even";
+    else cout<<"ODD";
+}
 
 //? What is output?
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int x=15;
-//     if(x<=15){
-//         cout<<"Inside if";
-//     }
-//     else if(x==15){
-//         cout<<"Inside else if";
-//     }
-//     cout<<x;
-// }
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x=15;
+    if(x<=15){
+        cout<<"Inside if";
+    }
+    else if(x==15){
+        cout<<"Inside else if";
+    }
+    cout<<x;
+}
 
 //? What is output?
 #include <iostream>
