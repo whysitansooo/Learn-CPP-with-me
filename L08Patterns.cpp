b82c2714b3c3883ef375d90f5aa34a58bde8aa3f
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2023-06-09 13:06:25 +0530

    Different types of pattern printing

--- L08Patterns.cpp
+++ L08Patterns.cpp
@@ -1,142 +1,142 @@
 //? Pattern 1
 
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int n;
-//     cout<<"Enter rows: ";
-//     cin>>n;
-//     int i = 1;
-//     while(i<=n){
-//         int j=1;
-//         while(j<=i){
-//             cout<<j<<' ';
-//             j++;
-//         }
-//         cout<<endl;
-//         i++;
-//     }
-// }
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter rows: ";
+    cin>>n;
+    int i = 1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<j<<' ';
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
 
 //? Pattern 2
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int n;
-//     cout<<"Enter number rows: ";
-//     cin>>n;
-//     int i = 1;
-//     int value = 1;
-//     while(i<=n){
-//         int j=1;
-//         while(j<=i){
-//             cout<<value<<' ';
-//             value++;
-//             j++;
-//         }
-//         cout<<endl;
-//         i++;
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter number rows: ";
+    cin>>n;
+    int i = 1;
+    int value = 1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<value<<' ';
+            value++;
+            j++;
+        }
+        cout<<endl;
+        i++;
 
-//     }
-//     }
+    }
+    }
 
 //? Pattern 3
 
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int n;
-//     cout<<"Enter number of row: ";
-//     cin>>n;
-//     int i=1;
-//     int value=1;
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter number of row: ";
+    cin>>n;
+    int i=1;
+    int value=1;
 
-//     while(i<=n){
-//     int space =1;
-//     while(space<=n-i){
+    while(i<=n){
+    int space =1;
+    while(space<=n-i){
 
-//         cout<<" ";
-//         space++;
-//     }
-//     int j=1;
-//     while(j<=i){
-//         cout<<value;
-//         value++;
-//         j++;
-//     }
-//     cout<<endl;
-//     i++;
-//     }
-// }
+        cout<<" ";
+        space++;
+    }
+    int j=1;
+    while(j<=i){
+        cout<<value;
+        value++;
+        j++;
+    }
+    cout<<endl;
+    i++;
+    }
+}
 
 //? Pattern 4 hard
 
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int n;
-//     cout << "Enter row: ";
-//     cin >> n;
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter row: ";
+    cin >> n;
 
-//     int val;
-//     for (int i = 1; i <= n; i++)
-//     {
-//         val = i;
-//         for (int j = 1; j <= i; j++)
-//         {
-//             cout<<val;
-//             val++;
-//         }
-//         cout << endl;
-//     }
-// }
+    int val;
+    for (int i = 1; i <= n; i++)
+    {
+        val = i;
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<val;
+            val++;
+        }
+        cout << endl;
+    }
+}
 
 //? Pattern 5 hard
 
-// #include<iostream>
-// using namespace std;
-// int main()
-// {
-//     int n;
-//     cin>>n;
-//     for(int i=1;i<=n;i++){
-//         int val=i;
-//         for(int space=i;space<=n-1;space++){
-//             cout<<" ";
-//         }
-//         for(int j=1;j<=i;j++){
-//             cout<<val;
-//             val++;
-//         }
-//         cout<<endl;
-//     }
-// }
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        int val=i;
+        for(int space=i;space<=n-1;space++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<val;
+            val++;
+        }
+        cout<<endl;
+    }
+}
 
 //? Pattern 6 star pattern
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
 
-// int main(){
+int main(){
 
-//    int n;
-//    cin>>n;
+   int n;
+   cin>>n;
 
-//    for(int i=1;i<=n;i++){
-// 	   for(int space=i;space<=n-1;space++){
-// 		   cout<<" ";
-// 	   }
-// 	   for(int j=1;j<=i*2-1;j++){
-// 		   cout<<"*";
-// 	   }
-// 	   cout<<endl;
-//    }
+   for(int i=1;i<=n;i++){
+	   for(int space=i;space<=n-1;space++){
+		   cout<<" ";
+	   }
+	   for(int j=1;j<=i*2-1;j++){
+		   cout<<"*";
+	   }
+	   cout<<endl;
+   }
   
-// }
+}
 
 #include<iostream>
 #include<cmath>
