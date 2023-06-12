f8074a6603828e556a6a4d12fd4391a95d95ecab
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2023-06-12 20:45:34 +0530

    Learn about Bitwise operators

---
+++ L10BitwiseOps.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+     int bor = 15|30;
+     int band = 15&30;
+     int bnot = ~15;
+     int bxor = 15^30;
+     int ls = 15<<2;
+     int rs = 15>>2;
+
+     cout<<bor<<endl;
+     cout<<band<<endl;
+     cout<<bnot<<endl;
+     cout<<bxor<<endl;
+     cout<<ls<<endl;
+     cout<<rs;
+
+     //! Increment Decrement ops
+    int a=1;
+    cout<<a++<<endl;
+    cout<<a<<endl;
+
+    
+
+
+
+
+    }
