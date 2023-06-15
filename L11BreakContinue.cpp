acfafe4a8faafef52fbe1ac5fce88f3886afae96
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2023-06-15 10:16:07 +0530

    Use of Break and Continue

---
+++ L11BreakContinue.cpp
@@ -0,0 +1,35 @@
+//? Prime number - Break 
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cout<<"Enter ur number: ";
+//     cin>>n;
+//     bool divide = false;
+//     for(int i=2;i<n;i++){
+//         if(n%i==0){
+//             divide = true;
+//             break;
+//         }
+//     }
+//     if(divide) cout<<"Not prime";
+//     else cout<<"Prime";
+// }
+
+
+//? Continue
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int j=1;j<10;j++){
+        if(j==7){
+            continue;
+        }
+        cout<<j<<endl;
+    }
+    
+}
