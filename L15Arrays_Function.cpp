1db5e789be9bcbfc29e3ab89498926ed7dc863c3
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2023-06-17 22:37:31 +0530

    Add files via upload

---
+++ L15Arrays_Function.cpp
@@ -0,0 +1,40 @@
+// #include <iostream>
+// using namespace std;
+
+// void print(int array[]){
+//     for(int i=0;i<5;i++){
+//         cout<<array[i]<<endl;;
+//     }
+// }
+
+// int main()
+// {
+//     int arr[5] = {1,2,3,4,5};
+//     print(arr);
+// }
+
+//? Reversing Array
+
+#include <iostream>
+using namespace std;
+
+void reverse(int n,int arr[]){
+    for(int i=0,j=n-1;j>i;i++,j--){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+}
+
+int main()
+{
+    int arr[5];
+    for(int i=0;i<5;i++){
+        cin>>arr[i];
+    }
+    reverse(5,arr);
+
+}
