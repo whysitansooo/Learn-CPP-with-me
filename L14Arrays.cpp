d02c0aafc7185d85536db58a8cf7e4ea2edf000c
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2023-06-19 22:53:33 +0530

    Learn about Arrays

--- L14Arrays.cpp
+++ L14Arrays.cpp
@@ -1,42 +1,42 @@
-// #include<iostream>
-// using namespace std;
-// int main()
-// {   
-//     //Taking array input
-//     int n;
-//     cin>>n;
+#include<iostream>
+using namespace std;
+int main()
+{   
+    //Taking array input
+    int n;
+    cin>>n;
 
-//     int arr[100];
-//     for(int i=0;i<n;i++){
-//         cin>>arr[i];
-//     }
-//     for(int i=0;i<n;i++){
-//         cout<<arr[i]<<endl;
-//     }
+    int arr[100];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
 
    
     
 
-// }
+}
 
-//? Largest element in an array
+? Largest element in an array
 
-// #include <iostream>
-// #include<climits>
-// using namespace std;
-// int  main()
-// {
-//     int arr[5];
-//     for(int i=0;i<5;i++){
-//         cin>>arr[i];
-//     }
-//     int max=INT_MIN;
-//     for(int i=0;i<5;i++){
-//         if(arr[i]>max) max=arr[i];
-//     }
-//     cout<<"MAX:"<<max;
+#include <iostream>
+#include<climits>
+using namespace std;
+int  main()
+{
+    int arr[5];
+    for(int i=0;i<5;i++){
+        cin>>arr[i];
+    }
+    int max=INT_MIN;
+    for(int i=0;i<5;i++){
+        if(arr[i]>max) max=arr[i];
+    }
+    cout<<"MAX:"<<max;
 
-// }
+}
 
 //! This is how arrays stored 
 
