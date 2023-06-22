a66cb3e322e609269c58386a302a63092d7983ba
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2023-06-22 22:31:56 +0530

    Binary search code

---
+++ L16BinarySearch.cpp
@@ -0,0 +1,44 @@
+//? This is my OG approach 
+
+#include <iostream>
+using namespace std;
+
+
+int binarySearch(int nums[], int left, int right, int target) {
+    
+    for(int i=0;i<10000;i++){
+        int mid = (left+right)/2;
+        if(nums[mid]==target) return mid;
+
+        else if(nums[mid]>target){
+            
+            right--;
+        }
+        else if(nums[mid]<target){
+            left++;
+        }
+        if(left>right) return -1;
+
+    }
+    
+}
+
+int main()
+{
+    int N;
+    cin>>N;
+    int A[N];
+    
+    for(int i=0;i<N;i++){
+        cin>>A[i];
+    }
+
+    int left=0;
+    int right=N-1;
+    
+    int target;
+    cin>>target;
+
+    int ans = binarySearch(A,0,N-1,target);
+    cout<<ans;
+}
