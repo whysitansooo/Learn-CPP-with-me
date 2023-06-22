0ef42ad3dddfe0dfe61719046ba56d5e476da243
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2023-06-22 22:32:41 +0530

    Different approaches of binary search

--- L16BinarySearch.cpp
+++ L16BinarySearch.cpp
@@ -42,3 +42,21 @@ int main()
     int ans = binarySearch(A,0,N-1,target);
     cout<<ans;
 }
+
+//? Another approach 
+
+int binarySearch(int nums[], int left, int right, int target) {
+    // Write Your Code Here
+    for(;left<=right;){
+        int mid = (left+right)/2;
+
+        if(nums[mid]==target) return mid;
+
+        else if(target<nums[mid]){
+            right = mid-1;
+        }
+        
+        else left  = mid+1;
+    }
+    return -1;
+}
