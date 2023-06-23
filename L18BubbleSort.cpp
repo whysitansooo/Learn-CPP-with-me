1fa8a66f731698e84172004d76c8796af6f9bb53
Sitansu Sekhar <118673403+whysitansooo@users.noreply.github.com>
2023-06-24 01:20:25 +0530

    Bubble Sorting and a question

--- L18BubbleSort.cpp
+++ L18BubbleSort.cpp
@@ -1,38 +1,38 @@
 // //? My approach
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// void BubbleSort(int arr[],int n){
+void BubbleSort(int arr[],int n){
     
-//         for(int i=0;i<n-1;i++){
-//             for(int j=0;j<n;j++){
-//                 if(arr[j]>arr[j+1]){
-//                     int temp = arr[j];
-//                     arr[j] = arr[j+1];
-//                     arr[j+1] = temp;
-//                 }
-//             }
-//         }
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
 
-// }
+}
 
-// int main()
-// {
-//     int n;
-//     cin>>n;
+int main()
+{
+    int n;
+    cin>>n;
 
-//     int arr[n];
-//     for(int i=0;i<n;i++){
-//         cin>>arr[i];
-//     }
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
 
-//     BubbleSort(arr,n);
+    BubbleSort(arr,n);
 
-//     for(int i=0;i<n;i++){
-//         cout<<arr[i];
-//     }
-// }
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+}
 
 
 //? Find Kth Smallest
